5c9cfc4d8c732558e6950417d71b36b84ff2048e
tkosht <takehito.oshita.business@gmail.com>
2023-09-03 20:54:37 +0900

    mod: updated

---
+++ bin/run.sh
@@ -0,0 +1,6 @@
+
+
+# docker run -it --rm render bash -i -c 'SHELL=/usr/bin/bash poetry run bash bin/run_demo.sh'
+# docker run -it --rm render bash
+docker run -it --rm render /home/devuser/.local/bin/poetry run bash bin/run_demo.sh
+
--- bin/run_demo_code_interpreter.sh
+++ bin/run_demo_code_interpreter.sh
@@ -5,5 +5,6 @@ cd $d/../
 
 export SHOW_INFO=False
 export SHELL=/usr/bin/bash
+export PATH="$HOME/.local/bin:$PATH"
 poetry run streamlit run app/codeinterpreter/executable/demo_stream.py --server.port 8501
 
